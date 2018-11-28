151a82af9f014741676d180825362ee4c5185975
Thomas Dornberger <ga84bib@mytum.de>
2018-11-28 17:31:03 +0100

    move Code to create faultyVersions to own project, link with shared project

---
+++ src/fault_selection/PitFaultSelectionStrategyBase.java
@@ -0,0 +1,28 @@
+package fault_selection;
+
+import java.util.List;
+import java.util.Set;
+
+import pit.data_objects.PitMutation;
+import pit.data_objects.PitTestCase;
+
+/**
+ * Builds faulty version for a pit project that should be analyzed.
+ * A faulty version means to select a subset of faults out of all faults of the project.
+ */
+public abstract class PitFaultSelectionStrategyBase {
+	protected int minFaultCount;
+	protected int maxFaultCount;
+	protected int versionsPerFaultCount;
+	public PitFaultSelectionStrategyBase(int minFaultCount,
+			int maxFaultCount, int versionsPerFaultCount) {
+		this.minFaultCount = minFaultCount;
+		this.maxFaultCount = maxFaultCount;
+		this.versionsPerFaultCount = versionsPerFaultCount;
+	}
+	/**
+	 * Selects versionsPerFaultCount * (maxFaultCount - minFaultCount + 1) faulty versions
+	 * according to the concrete strategy.
+	 */
+	public abstract List<Set<PitMutation>> selectFaultyVersions(List<PitMutation> pitFaults, List<PitTestCase> pitTests);
+}
